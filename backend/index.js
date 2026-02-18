92f56a2269d9a2601735a6a960495f7a3ad6681e
Adarsh165787 <adarshkumr1657@gmail.com>
2026-02-18 14:42:13 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -185,5 +185,5 @@ app.get("/employees/search", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`server running at localhost:${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 });
